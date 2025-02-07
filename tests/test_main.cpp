c5af20c6b479998117199206ae1d0d15727af103
Radu Iulian <iulianrad21@outlook.com>
2025-02-07 12:22:17 +0200

    Some quick python test setup

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,9 +1,21 @@
+#define PY_SSIZE_T_CLEAN
+#include <cstddef>
 #include <gtest/gtest.h>
+#include <Python.h>
+#include <pythonrun.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+
+int main(int argc, char** argv)
+{
+    Py_Initialize();
+
+    //PyRun_SimpleString("print(\"hello from python\")");
+    const auto fd= fopen("test.py","rb");
+    PyRun_SimpleFile(fd, "test.py");
+    testing::InitGoogleTest(&argc, argv);
+    const auto result = RUN_ALL_TESTS();
+
+    Py_FinalizeEx();
+
+    return result;
 }
