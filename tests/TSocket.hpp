f7a44e78fae29b508e09b1e45073de2299908f0d
Radu Iulian <iulianradu21@outlook.com>
2025-02-12 14:40:30 +0200

    Poll tests

--- tests/TSocket.hpp
+++ tests/TSocket.hpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <gtest/gtest.h>
 #include <thread>
+#include <random>
 
 #include "python_utils.hpp"
 #include "Socket.h"
@@ -14,6 +15,7 @@ namespace sfw_test
     {
     protected:
         TSocket()
+            : m_dist(1,10)
         {
             try
             {
@@ -42,6 +44,87 @@ namespace sfw_test
         PythonClassInstance m_client_test_instance;
     };
     
+    TEST_F(TSocket, PollTest)
+    {
+        using namespace std::chrono_literals;
+        std::atomic_bool stop = false;
+        std::atomic_bool poll_result = false;
+
+        /*
+        * Simple case of connecting to the socket and checking that the Poll method returns true 
+        */
+        {
+            iu::Socket s;
+            s.Listen("127.0.0.1", 12345, 0);
+            
+            std::thread t ([&s, &stop, &poll_result]
+            {
+                while(!stop)
+                {
+                    if (s.Poll() == true)
+                        poll_result = true;
+                }
+            });
+                
+            m_client_test_instance.CallMethod("PollConnect");
+            stop = true;
+            t.join();
+            
+            EXPECT_TRUE(poll_result);
+        }
+
+        /*
+        * Continuously polling to check that if nobody tries to connect the result is always false
+        */
+        {
+            iu::Socket s;
+            s.Listen("127.0.0.1", 12345, 0);
+            stop = false;
+
+            std::thread t([&]()
+            {
+                while(stop)
+                {
+                    EXPECT_FALSE(s.Poll());
+                }
+            });
+
+            // Allow some time to pass
+            std::this_thread::sleep_for(10s);
+            stop = true;
+            t.join();
+        }
+        
+        //The next few tests check the timout of the Poll method
+        {
+            iu::Socket s;
+            s.Listen("127.0.0.1", 12345, 0);
+            std::thread t([&]()
+            {
+                // TODO: change the interface to accept std::duration for now it is milliseconds
+                EXPECT_TRUE(s.Poll(2000));
+            });
+
+            std::this_thread::sleep_for(1s);
+            m_client_test_instance.CallMethod("PollConnect");
+            t.join();
+        }
+
+        {
+            iu::Socket s;
+            s.Listen("127.0.0.1", 12345, 0);
+            std::thread t([&]()
+            {
+                // TODO: change the interface to accept std::duration for now it is milliseconds
+                EXPECT_FALSE(s.Poll(1000));
+            });
+
+            std::this_thread::sleep_for(2s);
+            m_client_test_instance.CallMethod("PollConnect");
+            t.join();
+        }
+    }
+
     TEST_F(TSocket, ListenAccept)
     {
         std::atomic_bool has_accepted = false;
